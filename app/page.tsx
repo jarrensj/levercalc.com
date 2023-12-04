6855544c54f6ea122ad90df4f19cf09335f59e52
Jarren San Jose <sanjose.jarren@gmail.com>
2023-12-04 08:52:53 -0800

    add footer and disclaimer

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,14 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <main className="flex flex-col flex-grow  items-center p-24 text-center">
         <h1 className="text-4xl mb-2">calculator</h1>
+        <p className="text-xs mb-2">
+          Disclaimer: This is not financial advice. Leverage can be disasterous. This is just a calculator.
+        </p>
         <Calculator />
       </main>
+      <footer className="text-center p-4">
+        <Link href="https://omakasemoney.com">omakase money</Link>
+      </footer>
     </div>
   )
 }
