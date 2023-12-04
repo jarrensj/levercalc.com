f9ed4844546c59aca50d6052b4576eb4404fd6f0
Jarren San Jose <sanjose.jarren@gmail.com>
2023-12-04 08:21:09 -0800

    create calculator

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Calculator from '../components/Calculator'
 import Link from 'next/link'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen">
       <main className="flex flex-col flex-grow  items-center p-24 text-center">
         <h1 className="text-4xl mb-2">calculator</h1>
+        <Calculator />
       </main>
     </div>
   )
---
+++ components/Calculator.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import { useState, useEffect, ChangeEvent } from 'react';
+
+const Calculator = () => {
+  const [ethValue, setEthValue] = useState<number | ''>('');
+  const [dailyInterestValue, setDailyInterestValue] = useState<number | ''>('');
+  const [daysValue, setDaysValue] = useState<number | ''>('');
+
+  const [result, setResult] = useState<number>(0);
+
+  useEffect(() => {
+    if (ethValue !== '' && dailyInterestValue !== '' && daysValue !== '') {
+      setResult(ethValue * (dailyInterestValue / 100) * daysValue);
+    } else {
+      setResult(0);
+    }
+  }, [ethValue, dailyInterestValue, daysValue]);
+
+  const handleEthInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setEthValue(value === '' ? '' : parseFloat(value));
+  };
+
+  const handleInterestInputChange = (e: ChangeEvent<HTMLInputElement>) => { 
+    const value = e.target.value;
+    setDailyInterestValue(value === '' ? '' : parseFloat(value));
+  }
+
+  const handleDaysInputChange = (e: ChangeEvent<HTMLInputElement>) => { 
+    const value = e.target.value;
+    setDaysValue(value === '' ? '' : parseFloat(value));
+  }
+
+  return (
+    <div>
+      <p className="text-sm mb-2">
+        type the 
+        <span className="text-blue-500"> eth (pay later amount) </span>
+        in the input box below
+      </p>
+      <input 
+       type="number"
+       className="mb-2 w-25 py-1 text-lg text-center border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+       value={ethValue}
+       onChange={handleEthInputChange}
+      />
+      <p className="text-sm mb-2">
+        type the 
+        <span className="text-blue-500"> daily interest % </span>
+        in the input box below
+      </p>
+      <input 
+       type="number"
+       className="mb-2 w-25 py-1 text-lg text-center border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+       value={dailyInterestValue}
+       onChange={handleInterestInputChange}
+      />
+      <p className="text-sm mb-2">
+        type the 
+        <span className="text-blue-500"> days </span>
+        in the input box below
+      </p>
+      <input 
+       type="number"
+       className="mb-2 w-25 py-1 text-lg text-center border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+       value={daysValue}
+       onChange={handleDaysInputChange}
+      />
+      {result > 0 && (
+        <div className="text-2xl mb-2">
+          <p className="text-sm mb-2">result</p>
+          <p className="text-sm mb-2">{result} eth</p>
+        </div>
+      )
+      }
+    </div>
+  );
+};
+
+export default Calculator;
