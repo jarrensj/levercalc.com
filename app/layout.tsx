53aad151d64f409e50b58a563bf057fe56cc34f1
Jarren San Jose <sanjose.jarren@gmail.com>
2023-12-05 14:28:52 -0800

    typo in calculator metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'caclulator',
+  title: 'calculator',
   description: 'calculator',
 }
 
